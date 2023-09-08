9f96e415dab0d4a9c8ee9768fa04afde3efa9957
Natebuilder <d272226@edu.curio.nl>
2023-09-08 11:56:36 +0200

    opties kiezen

--- Jailbreak Adventure/Program.cs
+++ Jailbreak Adventure/Program.cs
@@ -8,6 +8,21 @@
             Console.ReadLine();
             Console.WriteLine("Type 1 to play!!");
             Console.WriteLine("Type 2 for options to see the options you can use.");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 1)
+            {
+                Console.WriteLine("Welcome to Jailbreak to start this game you have to escape prison!!");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Here are the possible options for this game");
+                Console.WriteLine("walk");
+                Console.WriteLine("run");
+                Console.WriteLine("punch");
+                Console.WriteLine("kick");
+            }
         }
     }
 }
