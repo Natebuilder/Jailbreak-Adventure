60eb0fa434abc1e6b1b7c69e1b7105dfac8602e0
Natebuilder <d272226@edu.curio.nl>
2023-09-08 11:54:35 +0200

    default files

--- Jailbreak Adventure/Program.cs
+++ Jailbreak Adventure/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Jailbreak");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            Console.WriteLine("Type 1 to play!!");
+            Console.WriteLine("Type 2 for options to see the options you can use.");
         }
     }
 }
