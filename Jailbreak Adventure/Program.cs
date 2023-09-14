bff274e9e5663c71ddb7eadf176819c08b5f1006
Natebuilder <d272226@edu.curio.nl>
2023-09-14 15:25:26 +0200

    question 1 almost done

--- Jailbreak Adventure/Program.cs
+++ Jailbreak Adventure/Program.cs
@@ -1,75 +1,67 @@
-﻿namespace Jailbreak_Adventure
+﻿using System.Diagnostics.Tracing;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+namespace Jailbreak_Adventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            gametitle();
+        }
+        public static void gametitle()
+        {
             Console.WriteLine("Jailbreak");
             Console.WriteLine("Press enter to continue.");
-            Console.ReadLine();
-            Console.WriteLine("Type 1 to play!!");
-            Console.WriteLine("Type 2 for options to see the options you can use.");
-
-            int number = int.Parse(Console.ReadLine());
-
+            Console.WriteLine("Type instructions to see instructions.");
+            string word = Console.ReadLine();
+            Console.Clear();
             
-
-            if (number == 1)
+            while (word == "instructions")
             {
-                Console.WriteLine("Welcome to Jailbreak to start this game you have to escape prison!!");
+                Console.WriteLine("You are in prison. You have to escape it.");
+                Console.WriteLine("Press enter to go back.");
                 Console.ReadLine();
-                Console.WriteLine("Your first goal is to escape the prison.");
-                Console.ReadLine();
-                Console.WriteLine("You are in the yard of the prison.");
-                Console.ReadLine();
-                Console.WriteLine("You want to steal a keycard of a cop.");
-                Console.WriteLine("What are you gonna do?");
-
-                string action = Console.ReadLine();
-
-                if (action == "walk")
-                {
-                    Console.WriteLine("You walked into the cop. The cop saw you try to steal the keycard.");
-                    Console.ReadLine();
-                    Console.WriteLine("As punishedment you are back in your cel.");
-                    Console.ReadLine();
-                }
-                if (action == "run")
-                {
-                    Console.WriteLine("You ran into the cop. You were so fast that the cop didn't know you stole his keycard.");
-                    Console.ReadLine();
-                    Console.WriteLine("You are running away with the keycard.");
-                    Console.ReadLine();
-                    Console.WriteLine("The cop is chasing you.");
-                    Console.WriteLine("What are you gonna do?");
-                }
-                if (action == "punch")
-                {
-                    Console.WriteLine("You tried to punch the cop. Too bad the cop dodged it. You are arrested.");
-                    Console.ReadLine();
-                    Console.WriteLine("As punishedment you are back in your cel.");
-                    Console.ReadLine();
-                }
-                if (action == "kick")
-                {
-                    Console.WriteLine("You kicked the cop knock out and ran away with the keycard.");
-                    Console.ReadLine();
-                    Console.WriteLine("Other cops saw it and run to you.");
-                    Console.WriteLine("What are you gonna do?");
-                    Console.ReadLine();
-                }
-                Console.ReadLine();
-                
-
+                Console.Clear();
+                gametitle();
             }
-            if (number == 2)
+            question1();
+        }
+        public static void question1()
+        {
+            string choice;
+            Console.WriteLine("You are in prison. You want to escape it. You see a cop standing in the yard.");
+            Console.WriteLine("What are you gonna do?.");
+            Console.WriteLine("1. Punch him in the face.");
+            Console.WriteLine("2. Act like a scary witch.");
+            Console.WriteLine("3. Vomit in his face.");
+            Console.Write("Choice: ");
+            choice = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            switch (choice)
             {
-                Console.WriteLine("Here are the possible options for this game");
-                Console.WriteLine("walk");
-                Console.WriteLine("run");
-                Console.WriteLine("punch");
-                Console.WriteLine("kick");
+                case "3":
+                case "Vomit in face":
+                case "Vomit":
+                    {
+                        Console.WriteLine("You vomit in the cop's face. He got angry and shoot in your head.");
+                        Console.WriteLine("As result you are dead.");
+                        Console.WriteLine("Press enter to continue!");
+                        Console.ReadLine();
+                        gameOver();
+                        break;
+                    }
             }
+        }
+        public static void gameOver()
+        {
+            Console.WriteLine("You are dead try again");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+            gametitle();
         }
     }
 }
