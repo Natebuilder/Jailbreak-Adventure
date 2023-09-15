73caa8f688e0db873bdbfc990ff9cbbdf280f5ef
Natebuilder <d272226@edu.curio.nl>
2023-09-15 11:44:20 +0200

    adding score at the menu

--- Jailbreak Adventure/Program.cs
+++ Jailbreak Adventure/Program.cs
@@ -12,14 +12,16 @@ namespace Jailbreak_Adventure
         }
         public static void gameTitle()
         {
+            highScore.score();
             Console.WriteLine("Jailbreak");
             Console.WriteLine("Press enter to continue.");
             Console.WriteLine("Type instructions to see instructions.");
             string word = Console.ReadLine();
             Console.Clear();
-            
+        
             while (word == "instructions")
             {
+                highScore.score();
                 Console.WriteLine("You are in prison. You have to escape it.");
                 Console.WriteLine("Press enter to go back.");
                 Console.ReadLine();
---
+++ Jailbreak Adventure/highScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jailbreak_Adventure
+{
+    public class highScore
+    {
+        public static void score()
+        {
+            int score = 0;
+            Console.WriteLine($"Score: {score}\n");
+        }
+    }
+}
